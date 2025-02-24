a64e9edd923e4c861df94e5e7865a5d4f9be58d1
William Santana <euwilliamsantana@gmail.com>
2025-02-24 12:09:58 -0300

    feat: Main section Two

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image"
 import logo from "../assets/logo.svg"
 import arrowDown from "../assets/icon-arrow-down.svg"
-import imageEgg from "../assets/image-transform.jpg"
+import imageTransform from "../assets/image-transform.jpg"
+import imageStandOut from "../assets/image-stand-out.jpg"
+import imageGraphic from "../assets/image-graphic-design.jpg"
+import imagePhotography from "../assets/image-photography.jpg"
 import Link from "next/link";
 
 
@@ -12,7 +15,7 @@ export default function Home() {
         <div className="p-8 flex justify-between content-center ">
           <Image src={logo} width={124} height={124} alt="Logotipo" />
           <nav >
-            <ul className="flex gap-8 content-center list-none font-[family-name:--font-barlow] items-center">
+            <ul className="flex gap-8 content-center list-none font-[family-name:--font-barlow] items-center text-white">
               <li>
                 <a href="">About</a>
               </li>
@@ -31,25 +34,40 @@ export default function Home() {
           </nav>
         </div>
         <div className="flex flex-col items-center mt-10">
-          <h1 className="font-[family-name:--font-fraunces] text-5xl uppercase tracking-widest">we are creatives</h1>
+          <h1 className="font-[family-name:--font-fraunces] text-5xl uppercase tracking-widest text-white">we are creatives</h1>
           <Image src={arrowDown} width={36} height={114} alt="Seta para baixo" className="mt-20" />
         </div>
       </header>
       <main>
-        <section className="grid grid-cols-2 h-96">
+        <section className="grid grid-cols-2 h-[450px]">
           <div className="flex justify-center items-start flex-col mx-24 gap-6">
             <h2 className="font-[family-name:--font-fraunces] text-4xl">Transform your brand</h2>
             <p className="text-sm text-gray-500 font-[family-name:--font-barlow]" >We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the markenting for you.</p>
             <Link href={"#"} className="font-[family-name:--font-fraunces] uppercase text-xs underline decoration-yellow-400 ">learn more</Link>
           </div>
           <div className="h-full relative w-full bg-[#ffd302]">
-            <Image src={imageEgg} alt="" fill className="object-contain" />
+            <Image src={imageTransform} alt="" fill className="object-contain" />
           </div>
+        </section>
+
+        <section className="grid grid-cols-2 h-[450px]">
+          <div className="h-full relative w-full bg-[#ff8071] ">
+            <Image src={imageStandOut} alt="" fill className="object-contain" />
+          </div>
+          <div className="flex justify-center items-start flex-col mx-24 gap-6">
+            <h2 className="font-[family-name:--font-fraunces] text-4xl">Stand out to the right audience</h2>
+            <p className="text-sm text-gray-500 font-[family-name:--font-barlow]" >
+              Using a collaborative formula of designers, researchers, photographers, veiographers, and copywriters, we'll build and extend yout brand in digital places.
+            </p>
+            <Link href={"#"} className="font-[family-name:--font-fraunces] uppercase text-xs underline decoration-red-500 ">learn more</Link>
+          </div>
+
         </section>
 
 
 
       </main>
+      <footer className="mt-32"></footer>
     </>
   );
 }
