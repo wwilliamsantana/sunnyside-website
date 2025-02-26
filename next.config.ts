66c2194a7bb36def868fc0124917e69f72a2ae85
William Santana <euwilliamsantana@gmail.com>
2025-02-26 10:22:16 -0300

    feat: modify nextjs workflow

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
     domains: ['github.com', "images.unsplash.com", "plus.unsplash.com"],
   },
